8b26b52be0e297839489b6837b9a086554d9be1f
Jackson <matlab-plus-plus-noreply+laptop@fake.com>
2022-10-16 17:52:45 -0400

    Fixed bugs with the test driver input argument processing and findstr call so results will be properly compared now.

--- test/test-driver.bat
+++ test/test-driver.bat
@@ -3,7 +3,24 @@
 :: exit code of 0 if matching and nonzero otherwise.
 @echo off
 
-set errorlevel=
-%1 %~dp0%2 | findstr "^%3$"
+:: Replace forward slashes with backslashes in the
+:: path to the executable under test.
+set exe_under_test=%1
+set exe_under_test=%exe_under_test:/=\\%
 
+:: Remove extra double quotes from the test data
+:: input file and concatenate with parent folder.
+set test_data_file=%~dp0%2
+set test_data_file=%test_data_file:"=%
+
+:: Run the executable under test and compare the
+:: output with the expected value.
+set errorlevel=
+%exe_under_test% "%test_data_file%" | findstr /r /c:"^%3 *$" > nul
+
+:: Return the error code given by findstr; if
+:: the output from the executable under test
+:: matched the expected value, the return code
+:: will be 0 and ctest understands this means
+:: the test passed.
 exit /B %errorlevel%
