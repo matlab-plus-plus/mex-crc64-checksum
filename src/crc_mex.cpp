d7b3ff17fc5a4e0b079ce356b2b0333472ad80f7
Jackson <matlab-plus-plus-noreply+laptop@fake.com>
2022-10-16 01:51:32 -0400

    Refactored options dispatching to use a switch case block and added an option for computing the checksum on a directory.

--- src/crc_mex.cpp
+++ src/crc_mex.cpp
@@ -1,4 +1,5 @@
 #include "crc.hpp"
+#include "private/directory_checksum_mt.hpp"
 
 #include "mex.hpp"
 #include "mexAdapter.hpp"
@@ -15,16 +16,32 @@ public:
 		matlab::data::CharArray textInput = inputs[1];
 
 		crc::checksum_t outVal{ 0U };
-		if (opt == 0U) {
-			// Compute checksum of a file
-			std::filesystem::path fp{ textInput.toAscii() };
-			outVal = crc::calc_checksum(fp);
-		}
-		else if (opt == 1U) {
-			// Compute checksum of a string
-			std::stringstream ss;
-			ss << textInput.toAscii();
-			outVal = crc::calc_checksum(ss);
+		switch (opt)
+		{
+			case 0U :
+			{
+				// Compute checksum of a file
+				std::filesystem::path fp{ textInput.toAscii() };
+				outVal = crc::calc_checksum(fp);
+				break;
+			}
+			case 1U :
+			{
+				// Compute checksum of a string
+				std::stringstream ss;
+				ss << textInput.toAscii();
+				outVal = crc::calc_checksum(ss);
+				break;
+			}
+			case 2U :
+			{
+				// Compute checksum of a folder
+				std::filesystem::path fp{ textInput.toAscii() };
+				outVal = crc::calc_dir_checksum(fp);
+				break;
+			}
+			default :
+				break;
 		}
 
 		matlab::data::ArrayFactory factory;
