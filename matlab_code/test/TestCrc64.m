e2f644fe0e36b5f8a5ec3f7dc166b533b829bbc8
matlab-plus-plus <matlab-plus-plus-noreply+laptop@fake.com>
2023-12-18 22:46:57 -0500

    Added code to handle when the test text input must be read through a file (due to null characters).

--- matlab_code/test/TestCrc64.m
+++ matlab_code/test/TestCrc64.m
@@ -8,7 +8,22 @@ classdef TestCrc64 < matlab.unittest.TestCase
     
     methods (Test)
         function TestTextChecksum(TestCase, IODataPair)
-            ActChecksum = crc64.TextChecksum(IODataPair.input_text, "AsHexString", true);
+            import matlab.unittest.constraints.HasField
+            
+            TestCase.assumeThat(IODataPair, HasField("input_text") | HasField("input_file"), ...
+                "Test parameter must provide the input either directly or through a file.");
+            
+            % Text content containing null characters cannot be directly
+            % read-in through the .json interface because it is interpreted
+            % as null-termination, so we will need to read some data from
+            % files.
+            if isfield(IODataPair, "input_file")
+                InputText = fileread(IODataPair.input_file);
+            else
+                InputText = IODataPair.input_text;
+            end
+            
+            ActChecksum = crc64.TextChecksum(InputText, "AsHexString", true);
             TestCase.verifyEqual(ActChecksum, string(IODataPair.output_hex))
         end
     end
