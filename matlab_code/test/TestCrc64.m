2e39fd37716007122e5e33355bcacb08f0262a89
matlab-plus-plus <matlab-plus-plus-noreply+laptop@fake.com>
2023-12-18 23:18:36 -0500

    Added 18 more tests (2 failing) for the *file* functions.

--- matlab_code/test/TestCrc64.m
+++ matlab_code/test/TestCrc64.m
@@ -2,9 +2,22 @@ classdef TestCrc64 < matlab.unittest.TestCase
     % Tests of the functions crc64.FileChecksum, crc64.TextChecksum, and
     % crc64.FolderChecksum.
     
+    properties (Access = private)
+        WorkingFolder (1,1) string;
+    end
+    
     properties (TestParameter)
         IODataPair = jsondecode(fileread("checksum_test_data.json"));
         OutputFormatAsHex = struct("hex", true, "int", false);
+    end
+    
+    methods (TestClassSetup)
+        function CreateTempFolder(TestCase)
+            import matlab.unittest.fixtures.TemporaryFolderFixture
+            
+            TFF = TestCase.applyFixture(TemporaryFolderFixture());
+            TestCase.WorkingFolder = TFF.Folder;
+        end
     end
     
     methods (Test)
@@ -35,6 +48,48 @@ classdef TestCrc64 < matlab.unittest.TestCase
             ActChecksum = crc64.TextChecksum(InputText, "AsHexString", OutputFormatAsHex);
             TestCase.verifyEqual(ActChecksum, ExpChecksum)
         end
+        
+        function TestFileChecksum(TestCase, IODataPair, OutputFormatAsHex)
+            import matlab.unittest.constraints.HasField
+            
+            TestCase.assumeThat(IODataPair, HasField("input_text") | HasField("input_file"), ...
+                "Test parameter must provide the input either directly or through a file.");
+            
+            % Copy if input is already a file
+            if isfield(IODataPair, "input_file")
+                OrigFile = fullfile(fileparts(mfilename("fullpath")), IODataPair.input_file);
+                InputFile = TestCase.CopyFile(OrigFile);
+            else
+                InputFile = TestCase.CreateFile(IODataPair.input_text);
+            end
+            
+            % Need to convert the expected checksum to an int if we're not
+            % getting a hex string as output.
+            if OutputFormatAsHex
+                ExpChecksum = string(IODataPair.output_hex);
+            else
+                ExpChecksum = uint64(hex2int(IODataPair.output_hex));
+            end
+            
+            ActChecksum = crc64.FileChecksum(InputFile, "AsHexString", OutputFormatAsHex);
+            TestCase.verifyEqual(ActChecksum, ExpChecksum)
+        end
+    end
+    
+    methods (Access = private)
+        function NewFile = CreateFile(TestCase, Text)
+            NewFile = tempname(TestCase.WorkingFolder);
+            filewrite(Text, NewFile);
+        end
+        
+        function NewFile = CopyFile(TestCase, FileName)
+            import matlab.unittest.constraints.IsFile
+            
+            TestCase.assumeThat(FileName, IsFile());
+            
+            NewFile = tempname(TestCase.WorkingFolder);
+            copyfile(FileName, NewFile);
+        end
     end
 end
 
@@ -45,4 +100,13 @@ IntOut = bitor( ...
     uint64(hex2dec(HexStr(9:end))), ...
     'uint64' ...
 );
+end
+
+function filewrite(FileText, FilePath)
+% Writes the text to a file
+fid = fopen(FilePath, 'w');
+clean_CloseFile = onCleanup(@() fclose(fid));
+
+fwrite(fid, FileText);
+
 end
